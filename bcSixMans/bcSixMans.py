b3989649eb66a4a483bcbcd041021629c0929a5e
Benjamin Hunt <bjh1381@rit.edu>
2021-03-03 20:16:28 -0500

    react to confirm bulk role removal

--- bcSixMans/bcSixMans.py
+++ bcSixMans/bcSixMans.py
@@ -187,6 +187,11 @@ class BCSixMans(commands.Cog):
         discord_id = str(ctx.message.author.id)
         if discord_id in account_register:
             count = len(account_register[discord_id])
+            
+            prompt = "React to confirm removal of the following account(s):\n - " + "\n - ".join("{}: {}".format(acc[0], acc[1]) for acc in self._get_all_accounts(ctx, ctx.message.author))
+            if not await self._react_prompt(ctx, prompt, "No accounts have been removed."):
+                return False
+            
             del account_register[discord_id]
             await ctx.send(":white_check_mark: Removed **{}** accounts.".format(count))
         else:
