e53280ccb07540c2b5a9a4c68dbca03b085e17ed
Benjamin Hunt <bjh1381@rit.edu>
2021-03-05 19:42:22 -0500

    accounts referenced incorrectly

--- bcSixMans/bcSixMans.py
+++ bcSixMans/bcSixMans.py
@@ -106,7 +106,8 @@ class BCSixMans(commands.Cog):
         member = ctx.message.author
         lines = []
         for acc in await self._get_all_accounts(ctx, member):
-            lines.append("<https://ballchasing.com/player/{}/{}>".format(acc[0], acc[1]))
+            # lines.append("<https://ballchasing.com/player/{}/{}>".format(acc[0], acc[1]))
+            lines.append(acc)
         show_accounts = "**{}**, has registered the following accounts:\n - ".format(member.name) + "\n - ".join(lines)
         await ctx.send("Here are the ballchasing pages for your reigistered accounts: {}".format())
 
