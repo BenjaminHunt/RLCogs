4b10a653b3b08185ccf911faa57f2dff72f77ce7
Benjamin Hunt <bjh1381@rit.edu>
2021-03-07 05:21:09 -0500

    troubleshooting no bc games found

--- bcSixMans/bcSixMans.py
+++ bcSixMans/bcSixMans.py
@@ -47,7 +47,7 @@ class BCSixMans(commands.Cog):
                 params = [
                     'uploader={}'.format(steam_id),
                     'playlist=private',
-                    'replay-date-after={}'.format(qpt),
+                    # 'replay-date-after={}'.format(qpt),
                     'count={}'.format(5),
                     # 'sort-by={}'.format(sort),
                     # 'sort-dir={}'.format(sort_dir)
