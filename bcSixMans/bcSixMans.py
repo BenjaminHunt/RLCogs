e58171fa2b9c8e845aee93fa077c76225fe74cb9
Benjamin Hunt <bjh1381@rit.edu>
2021-03-07 19:12:47 -0500

    gg takes games played - returns replay_ids/summary if it is found. parses platform, plat_id from account in account_register.

--- bcSixMans/bcSixMans.py
+++ bcSixMans/bcSixMans.py
@@ -509,6 +509,7 @@ class BCSixMans(commands.Cog):
         # swap_teams covers the scenario where the teams join incorrectly, assumes group is correct (applies to score summary only)
         swap_teams = False
         for account in account_register[str(uploader.id)]:
+            platform, plat_id = account
             account_replay_team = self._get_account_replay_team(platform, plat_id, replay_data)
             if account_sm_team != account_replay_team:
                 swap_teams = True
