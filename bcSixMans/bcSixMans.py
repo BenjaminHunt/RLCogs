dd342282a39116d5383b53a7b413b48838248f23
Benjamin Hunt <bjh1381@rit.edu>
2021-03-09 01:37:36 -0500

    fixes mistyped variable

--- bcSixMans/bcSixMans.py
+++ bcSixMans/bcSixMans.py
@@ -462,8 +462,8 @@ class BCSixMans(commands.Cog):
             return False
 
         # determine winner
-        orange_goals = replay_data['orange']['goals'] if 'goals' in replay['orange'] else 0
-        blue_goals = replay_data['blue']['goals'] if 'goals' in replay['blue'] else 0
+        orange_goals = replay_data['orange']['goals'] if 'goals' in replay_data['orange'] else 0
+        blue_goals = replay_data['blue']['goals'] if 'goals' in replay_data['blue'] else 0
         if blue_goals > orange_goals:
             winner = 'blue'
         else:
