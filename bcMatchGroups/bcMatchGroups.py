aa8f41064caf31f9c0b419349721dda0073db3e8
Benjamin Hunt <bjh1381@rit.edu>
2021-05-21 22:54:47 -0400

    allows names to be searched with case-insensitive characters

--- bcMatchGroups/bcMatchGroups.py
+++ bcMatchGroups/bcMatchGroups.py
@@ -129,7 +129,7 @@ class BCMatchGroups(commands.Cog):
         team_role = None
         if team_name:
             for role in team_roles:
-                if team_name in role.name.split() or (len(role.name.split()) > 1 and team_name == role.name.split()[-1][1:-1]):
+                if team_name.lower() in [name.lower() for name in role.name.split()] or (len(role.name.split()) > 1 and team_name.lower() == (role.name.split()[-1][1:-1]).lower()):
                     team_role = role
         else:
             for role in team_roles:
