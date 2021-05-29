d5d9267e30d506b99a999840a494c739a96d4358
Benjamin Hunt <bjh1381@rit.edu>
2021-05-29 01:57:29 -0400

    account manager troubleshooting

--- accountManager/accountManager.py
+++ accountManager/accountManager.py
@@ -100,7 +100,6 @@ class AccountManager(commands.Cog):
                     account_register[str(member.id)] = [[platform, identifier]]
                 await self._save_account_register(account_register)
                 await ctx.send("Done.")
-            return
 
         if valid_account:
             username, appearances = valid_account
