ebbf298373b6ef5cf674b994e76306218645e2a7
Benjamin Hunt <bjh1381@rit.edu>
2021-05-29 01:36:10 -0400

    account manager troubleshooting

--- accountManager/accountManager.py
+++ accountManager/accountManager.py
@@ -444,6 +444,8 @@ class AccountManager(commands.Cog):
 
         appearances = 0
         username = None
+        await ctx.send(auth_token)
+        await ctx.send(data)
         if 'list' in data:
             for team_color in ['blue', 'orange']:
                 for player in data['list'][0][team_color]['players']:
