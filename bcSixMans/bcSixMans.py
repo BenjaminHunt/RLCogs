c0be08099651c4157795a105396646ab8ba7f65e
bjh1381 <bjh1381@ad.rit.edu>
2021-06-03 09:27:46 -0400

    only runs update if the guild has a top level group set

--- bcSixMans/bcSixMans.py
+++ bcSixMans/bcSixMans.py
@@ -119,6 +119,12 @@ class BCSixMans(commands.Cog):
         self.six_mans_cog.add_observer(self)
 
     async def update(self, game):
+        try:
+            guild = game.textChannel.guild
+            if not await self._get_top_level_group(guild):
+                return
+        except:
+            return
         await game.textChannel.send("State: {}".format(game.game_state))
         if game.game_state == "game over":
             await self._process_six_mans_replays(game)
