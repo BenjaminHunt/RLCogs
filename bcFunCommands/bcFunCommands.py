cd77dbf9d363f8ad534b53aaaaed5adfd5074f50
Ben Hunt <bjh1381@rit.edu>
2022-02-23 14:52:51 -0500

    more embed info

--- bcFunCommands/bcFunCommands.py
+++ bcFunCommands/bcFunCommands.py
@@ -48,11 +48,12 @@ class BCFunCommands(commands.Cog):
             return await ctx.send(":x: No recent replays found")
 
         json_replays.sort(key=lambda replay: replay["date"])
-        full_replay_json = self.get_full_replay_json(token, json_replays[0]['id'])
+        target_replay_id = json_replays[0]['id']
+        full_replay_json = self.get_full_replay_json(token, target_replay_id)
         target_account = self.which_account_in_full_replay(full_replay_json, accounts)
         player_data = self.get_player_data_from_replay(full_replay_json, target_account[0], target_account[1])
 
-        embed = self.get_player_settings_embed(player, player_data)
+        embed = self.get_player_settings_embed(target_replay_id, player, player_data)
 
         await ctx.send(embed=embed)
 
@@ -157,7 +158,7 @@ class BCFunCommands(commands.Cog):
                 return role.color
         return None
 
-    def get_player_settings_embed(self, member, player_data):
+    def get_player_settings_embed(self, replay_id, member, player_data):
         member_color = self.get_member_color(member)
 
         embed = discord.Embed(
@@ -176,7 +177,16 @@ class BCFunCommands(commands.Cog):
         
         cam_str = "```\n{}\n```".format('\n'.join(cam_settings_list))
 
-        embed.description = cam_str
+        name = player_data.get('name')
+        platform = player_data['id']['platform']
+        plat_id = player_data['id']['id']
+        player_page_link = f'https://ballchasing.com/player/{platform}/{plat_id}'
+
+        embed.add_field(name="Account", value=f"[{platform} | {name}]({player_page_link})", inline=False)
+        embed.add_field(name="Camera Settings", value=cam_str, inline=False)
+        embed.add_field(name="Replay", value=f"[Click Here to view](https://ballchasing.com/replay/{replay_id})", inline=False)
+
+        # embed.description = cam_str
 
         return embed 
 
