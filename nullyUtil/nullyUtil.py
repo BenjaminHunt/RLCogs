1a84b86d1c568d5c9cfe6aeaee81e2386940c450
Benjamin Hunt <bjh1381@rit.edu>
2022-09-06 21:15:06 -0400

    OH WHOOPS LOL. Should be good now.

--- nullyUtil/nullyUtil.py
+++ nullyUtil/nullyUtil.py
@@ -1,31 +1,36 @@
 import discord
 
-import asyncio
-
+from discord.ui import ActionRow, Button, ButtonStyle
 from redbot.core import Config, commands, checks
 from redbot.core.utils.predicates import ReactionPredicate
 from redbot.core.utils.menus import start_adding_reactions
 
-from datetime import datetime, timedelta
-from pytz import timezone, all_timezones_set
-
-defaults = {}
+defaults = {'TimeZone': 'America/New_York'}
 
 
 WHITE_X_REACT = "\U0000274E"
 
-class NullyUtil(commands.Cog):
-    """NullyUtil commands for anything!"""
+class TestCog(commands.Cog):
+    """Test misc commands for anything!"""
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(
             self, identifier=1234567893, force_registration=True)
         self.config.register_guild(**defaults)
-        self.time_zones = {}
 
-        self.task = asyncio.create_task(self.pre_load_data())
-        inter_client = InteractionClient(bot)
 
-    @commands.command()
-    async def test(self, ctx):
+    @commands.guild_only()
+    @commands.command(aliases=['addEmote', 'addemoji', 'addemote'])
+    @checks.admin_or_permissions(manage_guild=True)
+    async def addEmoji(self, ctx, emoji: discord.Emoji, *, emoji_name=None):
+        guild : discord.Guild = ctx.guild
+        if len(guild.emojis) >= guild.emoji_limit:
+            return await ctx.reply(":x: The guild has already met its emoji limit.")
         
+        if not emoji_name:
+            emoji_name = emoji.name
+
+        emoji_bytes : bytes = await emoji.read()
+        await guild.create_custom_emoji(name=emoji_name, image=emoji_bytes)
+        await ctx.reply("Done")
+
